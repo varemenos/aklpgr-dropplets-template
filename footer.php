4700d4fa7340dacb6cb4b41a023fb12e2cf06ead
Adonis K <aklp08@gmail.com>
2013-09-27 10:14:27 +0300

    added syntax highlighting by using Lea Verou's prism

--- footer.php
+++ footer.php
@@ -2,3 +2,5 @@
 <footer>
 	<p><?php echo $blog_title . ' ' . '&copy; ' . date('Y'); ?></p>
 </footer>
+
+<script src="<?php echo $blog_url; ?>templates/<?php echo $template; ?>/assets/js/prism.js"></script>
