01678a98ac72bdc940186327bfc8fc4e2e2cc6ed
Adonis K <aklp08@gmail.com>
2013-10-03 00:52:37 +0300

    fix nav spacing

--- configuration.php
+++ configuration.php
@@ -34,21 +34,21 @@
 	$navigation[0] = array(
 		"url" => BLOG_URL,
 		"external" => false,
-		"content" => "<i class='icon-home'></i> Home",
+		"content" => "<i class='icon-home'></i>Home",
 		"children" => false
 	);
 
 	$navigation[1] = array(
 		"url" => false,
 		"external" => false,
-		"content" => "<i class='icon-tags'></i> Categories",
+		"content" => "<i class='icon-tags'></i>Categories",
 		"children" => getCategories()
 	);
 
 	$navigation[2] = array(
 		"url" => BLOG_URL . "rss",
 		"external" => true,
-		"content" => "<i class='icon-feed'></i> RSS",
+		"content" => "<i class='icon-feed'></i>RSS",
 		"children" => false
 	);
 ?>
