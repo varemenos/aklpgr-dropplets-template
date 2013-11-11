02199563a5133534a3bdda053f9eedda45ff223e
Adonis K <aklp08@gmail.com>
2013-11-11 23:13:43 +0200

    added analytics tracking snippet

--- footer.php
+++ footer.php
@@ -4,3 +4,13 @@
 </footer>
 
 <script src="<?php echo $blog_url; ?>templates/<?php echo $template; ?>/assets/js/prism.js"></script>
+
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-18251681-1');  // Replace with your property ID.
+	ga('send', 'pageview');
+</script>
